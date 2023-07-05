9c45225ed15bf0afe92a6f8517ace92f5248ce82
gtea <liliang_subu@163.com>
2023-07-05 08:59:46 +0800

    新增clang-tidy并修改警告

--- android_ninja_to_compdb.cpp
+++ android_ninja_to_compdb.cpp
@@ -8,8 +8,94 @@
 #include <regex>
 #include <string>
 #include <vector>
+#include <array>
 
-using namespace std;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::ios;
+
+static constexpr int PATH_LEN = 256;
+static constexpr int JSON_UNIT_SIZE = 256 * 1024;
+static const string COMPDB_FILE_NAME_NAME{"compile_commands.json"};
+
+string generate_json_unit(const string &dir, const string &cmd, const string &opts)
+{
+    static int s_cnt{0};
+    string ret;
+
+    ret.reserve(JSON_UNIT_SIZE);
+
+    //dir
+    ret.append("  {\n");
+    ret.append(R"(    "directory": ")").append(dir).append("\",\n");
+
+    //cmd
+    std::vector<string> strs;
+    std::regex ws_re("\\s+");  // 空白符
+
+    // std::copy( std::sregex_token_iterator(input_str.begin(),
+    // input_str.end(), ws_re, -1),
+    //            std::sregex_token_iterator(),
+    //            strs.begin());
+    bool cat_flag = false;
+    for (auto itor = std::sregex_token_iterator(opts.begin(), opts.end(), ws_re, -1);
+         itor != std::sregex_token_iterator(); itor++)
+    {
+        if (itor->compare("-fdebug-prefix-map=\\$$PWD/=") == 0)
+        {
+            continue;
+        }
+
+        if (itor->compare("\\$$(cat") == 0)
+        {
+            cat_flag = true;
+            continue;
+        }
+
+        if (cat_flag)
+        {
+            cat_flag = false;
+            string tmp = *itor;
+            tmp.pop_back();
+
+            ifstream stream_extra;
+            stream_extra.open(tmp, ios::in);
+
+            string tmp_line;
+            string tmp_total_str;
+            while (getline(stream_extra, tmp_line))
+            {
+                tmp_total_str.append(tmp_line).append(" ");
+            }
+
+            for (auto it2 = std::sregex_token_iterator(tmp_total_str.begin(), tmp_total_str.end(), ws_re, -1);
+                 it2 != std::sregex_token_iterator(); it2++)
+            {
+                strs.push_back(*it2);
+            }
+            continue;
+        }
+        strs.push_back(*itor);
+    }
+
+    ret.append(R"(    "command": ")").append(cmd);
+    for (auto &itor : strs)
+    {
+        ret.append(" ").append(itor);
+    }
+    ret.append("\",\n");
+
+    //file
+    ret.append(R"(    "file": ")").append(strs.back()).append("\"\n");
+    ret.append("  }");
+
+    cout << ++s_cnt << " ===> " << strs.back() << endl;
+    return ret;
+}
 
 int main(int argc, char *argv[])
 {
@@ -22,21 +108,22 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    char cur_path[512] = {0};
-    getcwd(cur_path, sizeof(cur_path));
+    std::array<char, PATH_LEN> cur_path;
+    getcwd(cur_path.data(), cur_path.size());
+    string dir{cur_path.data()};
     // cout << cur_path << endl;
 
-    string compdb_file{"compile_commands.json"};
-    ofstream stream_out(compdb_file);
-    if (stream_out.is_open() == false)
+    string COMPDB_FILE_NAME{"compile_commands.json"};
+    ofstream stream_out(COMPDB_FILE_NAME);
+    if (!stream_out.is_open())
     {
-        cerr << "open file " << compdb_file << " failed!" << endl;
+        cerr << "open file " << COMPDB_FILE_NAME << " failed!" << endl;
         return -1;
     }
 
     string ninja_file{argv[1]};
     ifstream stream_in(ninja_file, ios::in);
-    if (stream_in.is_open() == false)
+    if (!stream_in.is_open())
     {
         cerr << "open file " << ninja_file << " failed!" << endl;
         return -1;
@@ -48,10 +135,8 @@ int main(int argc, char *argv[])
     while (getline(stream_in, cur_line))
     {
         std::smatch result;
-        std::regex pattern(
-            "^ command = /bin/bash -c \"PWD=/proc/self/cwd.* "
+        std::regex pattern("^ command = /bin/bash -c \"PWD=/proc/self/cwd.* "
             "(\\S*/bin/(clang|clang\\+\\+)) (.*\\.(cpp|c))\"$");
-        // "prebuilts/\\S+ (.*\\.(cpp|c))\"$");
         if (std::regex_match(cur_line, result, pattern))
         {
             // for regex debug
@@ -68,76 +153,12 @@ int main(int argc, char *argv[])
             {
                 stream_out << "," << endl;  // 非首个cmd给上个cmd的}后增加一个,分隔符
             }
-            stream_out << "  {" << endl;
-            stream_out << "    \"directory\": \"" << cur_path << "\"," << endl;
 
-            string clang_path{result[1]};
-            string input_str{result[3]};
-
-            std::vector<string> strs;
-            std::regex ws_re("\\s+");  // 空白符
-
-            // std::copy( std::sregex_token_iterator(input_str.begin(),
-            // input_str.end(), ws_re, -1),
-            //            std::sregex_token_iterator(),
-            //            strs.begin());
-            bool cat_flag = false;
-            for (auto it = std::sregex_token_iterator(input_str.begin(), input_str.end(), ws_re, -1);
-                 it != std::sregex_token_iterator(); it++)
-            {
-                if (it->compare("-fdebug-prefix-map=\\$$PWD/=") == 0)
-                {
-                    continue;
-                }
-
-                if (it->compare("\\$$(cat") == 0)
-                {
-                    cat_flag = true;
-                    continue;
-                }
-
-                if (cat_flag)
-                {
-                    cat_flag = false;
-                    string tmp = *it;
-                    tmp.pop_back();
-                    // cout << tmp << endl;
-
-                    ifstream stream_extra;
-                    stream_extra.open(tmp, ios::in);
-
-                    string tmp_line;
-                    string tmp_total_str;
-                    while (getline(stream_extra, tmp_line))
-                    {
-                        tmp_total_str.append(tmp_line).append(" ");
-                    }
-
-                    for (auto it2 = std::sregex_token_iterator(tmp_total_str.begin(), tmp_total_str.end(), ws_re, -1);
-                         it2 != std::sregex_token_iterator(); it2++)
-                    {
-                        // cout << *it2 << endl;
-                        strs.push_back(*it2);
-                    }
-                    continue;
-                }
-                strs.push_back(*it);
-            }
-
-            stream_out << "    \"command\": \"" << clang_path;
-            for (auto &it : strs)
-            {
-                stream_out << " " << it;
-            }
-            stream_out << "\"," << endl;
-
-            stream_out << "    \"file\": \"" << strs.back() << "\"" << endl;
-            stream_out << "  }";
-
-            cout << ++cnt << " ===> " << strs.back() << endl;
+            stream_out << generate_json_unit(dir, result[1], result[3]);
         }
     }
 
     stream_out << endl << "]" << endl;
     return 0;
 }
+
