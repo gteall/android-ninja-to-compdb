e2ffa9e9d80452e0bd7422f824a093a80a5cb8fd
gtea <liliang_subu@163.com>
2023-08-19 15:30:17 +0800

    1.增加多线程支持 2.增加执行过程进度条显示

--- android_ninja_to_compdb.cpp
+++ android_ninja_to_compdb.cpp
@@ -2,35 +2,27 @@
 #include <sys/signal.h>
 #include <unistd.h>
 #include <termios.h>    // struct termios, tcgetattr(), tcsetattr()
-#include <sys/stat.h>
-
 #include <algorithm>
 #include <array>
 #include <chrono>
+#include <cstdio>
 #include <cstdlib>
 #include <fstream>
+#include <ios>
 #include <iostream>
 #include <iterator>
+#include <mutex>
 #include <regex>
 #include <string>
 #include <thread>
 #include <vector>
 #include "cmd_out.hpp"
 
-using std::endl;
-using std::ifstream;
-using std::ios;
-using std::ofstream;
 using std::string;
 
 static constexpr int PATH_LEN = 256;
 static constexpr int JSON_UNIT_SIZE = 256 * 1024;
-static const string COMPDB_FILE_NAME{"compile_commands.json"};
-
-namespace
-{
-ofstream s_stream_out;
-}  // namespace
+static const std::string COMPDB_FILE_NAME{"compile_commands.json"};
 
 
 void set_key_mode()
@@ -48,7 +40,7 @@ void set_key_mode()
     }
 }
 
-string generate_json_unit(const string &dir, const string &cmd, const string &opts)
+std::string generate_json_unit(const string &dir, const string &cmd, const string &opts)
 {
     static int s_cnt{0};
     string ret;
@@ -88,8 +80,8 @@ string generate_json_unit(const string &dir, const string &cmd, const string &op
             string tmp = *itor;
             tmp.pop_back();
 
-            ifstream stream_extra;
-            stream_extra.open(tmp, ios::in);
+            std::ifstream stream_extra;
+            stream_extra.open(tmp, std::ios::in);
 
             string tmp_line;
             string tmp_total_str;
@@ -123,22 +115,109 @@ string generate_json_unit(const string &dir, const string &cmd, const string &op
     return ret;
 }
 
-uint32_t get_file_size(const std::string &file)
+class InputFile
 {
-    uint32_t ret = 0;
-    struct stat file_stat;
-    if(0 == stat(file.c_str(), &file_stat))
+public:
+    InputFile(const std::string file)
     {
-        ret = file_stat.st_size;
+        _stream.open(file, std::ios::in);
+        if(!_stream.is_open())
+        {
+            gtea::CmdOut::print("open ifile %s failed", file.c_str());
+            return;
+        }
+
+        _stream.seekg(0, std::ios_base::end);
+
+        gtea::CmdOut::ProgressBarInfo bar_info;
+        bar_info.total = _stream.tellg();
+        gtea::CmdOut::createProgressBar(bar_info);
+
+        _stream.seekg(0, std::ios_base::beg);
+
     }
-    return ret;
-}
+
+    ~InputFile()
+    {
+        if(_stream.is_open())
+        {
+            _stream.close();
+        }
+    }
+
+    bool getLine(std::string &str)
+    {
+        bool ret = false;
+        std::unique_lock<std::mutex> lck(_mutex);
+        if(_stream.is_open() && getline(_stream, str))
+        {
+            _done_size += str.size() + 1;
+            gtea::CmdOut::updateProgressBar(_done_size);
+            ret = true;
+        }
+        return ret;
+    }
+
+private:
+    std::ifstream    _stream;
+    std::mutex  _mutex;
+    int         _done_size{0};
+};
+
+class OutputFile
+{
+public:
+    OutputFile(const string &filename)
+    {
+        _ostream.open(filename);
+        if(!_ostream.is_open())
+        {
+            gtea::CmdOut::print("open ofile %s failed!", filename.c_str());        
+        }
+        else
+        {
+            _ostream << '[' << std::endl;        
+        }
+    }
+
+    ~OutputFile()
+    {
+        if(_ostream.is_open())
+        {
+            _ostream << std::endl << ']' << std::endl;        
+            _ostream.close();
+        }
+    }
+
+    void write(const std::string &str)
+    {
+        std::unique_lock<std::mutex> lck(_mutex);
+        if (_ostream.is_open())
+        {
+            if (_first_flag)
+            {
+                _first_flag = false;
+            }
+            else
+            {
+                _ostream << "," << std::endl;
+            }
+            _ostream << str;
+        }
+    }
+
+private:
+    std::ofstream    _ostream;
+    std::mutex  _mutex;
+    bool        _first_flag{true};
+};
+
 
 int main(int argc, char *argv[])
 {
     bool running_flag = true;
-    bool first_cmd_flag = true;
     int cnt = 0;
+    int thread_num = 32; //常用服务器是64核的 默认开32个线程
 
     if (argc < 2)
     {
@@ -146,32 +225,18 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    if(argc >= 3)
+    {
+        thread_num = atoi(argv[2]);
+    }
+
     std::array<char, PATH_LEN> cur_path;
     getcwd(cur_path.data(), cur_path.size());
     string dir{cur_path.data()};
     // cout << cur_path << endl;
 
-    // string COMPDB_FILE_NAME{"compile_commands.json"};
-    // ofstream stream_out(COMPDB_FILE_NAME);
-    // if (!stream_out.is_open())
-    // {
-    //     cerr << "open file " << COMPDB_FILE_NAME << " failed!" << endl;
-    //     return -1;
-    // }
-    s_stream_out.open(COMPDB_FILE_NAME);
-    if (!s_stream_out.is_open())
-    {
-        gtea::CmdOut::print("open file %s failed!", COMPDB_FILE_NAME.c_str());
-        return -1;
-    }
-
-    string ninja_file{argv[1]};
-    ifstream stream_in(ninja_file, ios::in);
-    if (!stream_in.is_open())
-    {
-        gtea::CmdOut::print("open file %s failed!", ninja_file.c_str());
-        return -1;
-    }
+    OutputFile  ofile{COMPDB_FILE_NAME};
+    InputFile   ifile{argv[1]};
 
     //按q键中断ninja文件解析
     std::thread key_handler{[&running_flag](){
@@ -191,51 +256,54 @@ int main(int argc, char *argv[])
     }};
     key_handler.detach();
 
-    s_stream_out << "[" << endl;
+    const auto start = std::chrono::high_resolution_clock::now();
 
-    int file_size = get_file_size(ninja_file);
-    gtea::CmdOut::ProgressBarInfo bar_info;
-    bar_info.total = file_size;
-    gtea::CmdOut::createProgressBar(bar_info);
-    // gtea::ProgressBar bar{"", file_size};
-    int cur_size = 0;
+    std::vector<std::thread> threads;
 
-    string cur_line;
-    while (running_flag && getline(stream_in, cur_line))
+    for(int i = 0; i < thread_num; i++)
     {
-        cur_size += cur_line.size() + 1;
-        gtea::CmdOut::updateProgressBar(cur_size);
-        // bar.update(cur_size);
-        // std::cout << file_size << " : " << cur_size << std::endl;
-
-        std::smatch result;
-        std::regex pattern(
-            "^ command = /bin/bash -c \"PWD=/proc/self/cwd.* "
-            "(\\S*/bin/(clang|clang\\+\\+)) (.*\\.(cpp|c))\"$");
-        if (std::regex_match(cur_line, result, pattern))
+        std::thread thr([&](int index)
         {
-            // for regex debug
-            //  for(int i = 0; i < result.size(); i++)
-            //  {
-            //    cout << "result["  << i << "] = " << result[i] << endl;
-            //  }
-
-            if (first_cmd_flag)
+            int cnt = 0;
+            std::chrono::duration<double> difftime;
+            std::string cur_line;
+            while(ifile.getLine(cur_line))
             {
-                first_cmd_flag = false;
-            }
-            else
-            {
-                s_stream_out << "," << endl;  // 非首个cmd给上个cmd的}后增加一个,分隔符
+                const auto start = std::chrono::high_resolution_clock::now();
+                std::smatch result;
+                std::regex pattern(
+                    "^ command = /bin/bash -c \"PWD=/proc/self/cwd.* "
+                    "(\\S*/bin/(clang|clang\\+\\+)) (.*\\.(cpp|c))\"$");
+                if (std::regex_match(cur_line, result, pattern))
+                {
+                    ofile.write(generate_json_unit(dir, result[1], result[3]));
+                }
+                cnt++;
+
+                const auto end = std::chrono::high_resolution_clock::now();
+                difftime += (end - start);
+
             }
 
-            s_stream_out << generate_json_unit(dir, result[1], result[3]);
+            gtea::CmdOut::print("thread %d parse cnt %d %lfs", index, cnt, difftime.count());
+        }, i);
+        threads.push_back(std::move(thr));
+    }
 
-            // debug
-            // std::this_thread::sleep_for(std::chrono::seconds(2));
+    for(auto &it : threads)
+    {
+        if(it.joinable())
+        {
+            it.join();
         }
     }
 
-    s_stream_out << endl << "]" << endl;
+    const auto end = std::chrono::high_resolution_clock::now();
+    // const std::chrono::duration<double, std::milli> elapsed = end - start;
+    const std::chrono::duration<double> elapsed = end - start;
+ 
+    // std::cout << elapsed.count() << "s" << std::endl;
+    gtea::CmdOut::print("used time : %lfs", elapsed.count());
+
     return 0;
 }
